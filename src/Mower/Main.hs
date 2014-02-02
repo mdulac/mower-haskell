07c4f6c01fbd943e135e1058343e5087248b2c1f
mdulac <mat.dulac@gmail.com>
2014-02-02 01:46:24 +0100

    clean Main module

--- src/Mower/Main.hs
+++ src/Mower/Main.hs
@@ -1,7 +1,10 @@
 module Main where
 
 import System.IO
+
+import Data.Maybe
 import Control.Monad.State
+
 import Mower.Core
 
 -- Lazy eval
@@ -12,10 +15,7 @@ main :: IO ()
 main = do
 	handle <- openFile "resources/commands.txt" ReadMode
 	content <- hGetContents handle
-
-	let f = makeEmptyField 0 0
-	-- let b = execState (makeConfig $ withLineNumber $ (lines content)) (Board f [])
-
-	-- print $ execState (playGame (players b) ) (field b)
-
+	let emptyField = fromJust (makeEmptyField 0 0)
+	let board = execState (makeBoard $ withLineNumber $ (lines content)) (Board emptyField [])
+	print $ execState (playGame (players board) ) (field board)
 	hClose handle
